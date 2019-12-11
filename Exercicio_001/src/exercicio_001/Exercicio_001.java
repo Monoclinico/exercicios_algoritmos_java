701ea4ebcf10ca49b3f7bd0bbf2d608a919a2667
Matheus Sanches <matheussanchesleme@gmail.com>
2019-12-11 20:35:50 -0300

    Algoritmo que calcula o perímetro e área de um retangulo foi aperfeiçoado.

--- Exercicio_001/src/exercicio_001/Exercicio_001.java
+++ Exercicio_001/src/exercicio_001/Exercicio_001.java
@@ -1,31 +1,32 @@
 package exercicio_001;
 import java.util.Scanner;
-
 public class Exercicio_001 {
-
     public static void main(String[] args) {
         //Declaração de variaveis
-        Scanner teclado = new Scanner(System.in);
-        double ladoA;
-        double ladoB;
-        double perimetro;
-        double area;
+        double ladoA=0., ladoB=0., perimetro=0., area=0.;
         // Inicio do programa
         System.out.println("Calcula perimetro e area de um retangulo");
-        System.out.print("Entre o lado A: ");
-        ladoA = teclado.nextDouble();
-        System.out.print("Entre o lado B: ");
-        ladoB = teclado.nextDouble();   
+        while(true){
+            try{
+                Scanner teclado = new Scanner(System.in);        
+                System.out.print("Entre o lado A: ");
+                ladoA = teclado.nextDouble();
+                System.out.print("Entre o lado B: ");
+                ladoB = teclado.nextDouble();
+                break;
+            }catch(Exception e){
+                System.out.println("Algo deu errado na entrada de dados, "
+                        + "tente novamente.");
+            }
+        }
         //Processamento
-        perimetro = (2 * ladoA) + (2 * ladoB);
+        perimetro = (ladoA * 2) + (ladoB * 2);
         area = ladoA * ladoB;
         //Saida de dados
         System.out.println("Lado A = " + ladoA);
         System.out.println("Lado B = " + ladoB);
         System.out.println("Perimetro = " + perimetro);
         System.out.println("Area = " + area);
-
     }
-
 }
  
